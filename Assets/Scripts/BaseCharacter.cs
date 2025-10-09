82161fef6bac4d5e9ff9e3ddfe8164c622bd9a8e
Arthur Gonçalves <arthurga54@gmail.com>
2025-10-09 09:23:16 -0300

    iniciando player

--- Assets/Scripts/BaseCharacter.cs
+++ Assets/Scripts/BaseCharacter.cs
@@ -5,14 +5,15 @@ public abstract class BaseCharacter : MonoBehaviour
     public string Name { get; protected set; }
     public int Health { get; protected set; }
 
-    [SerializeField] protected float m_speed;
-    [SerializeField] protected float m_strength;
-    [SerializeField] protected bool m_isAlive;
+    [SerializeField] protected float m_Speed;
+    [SerializeField] protected float m_Strength;
+    [SerializeField] protected bool m_IsAlive;
 
     protected void Awake()
     {
         Setup();
     }
+
     protected abstract void Setup();
 
 
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,10 +1,17 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : BaseCharacter
 {
+    [SerializeField] private PlayerInput m_playerInput;
+
+    private Vector2 m_movementDiretion;
     protected override void Setup()
     {
+        m_playerInput.actions["Move"].performed += ctx => m_movementDiretion = ctx.ReadValue<Vector2>();
+        m_playerInput.actions["Move"].canceled += ctx => m_movementDiretion = Vector2.zero;
     }
+    private void Update() => transform.Translate(m_movementDiretion * m_Speed * Time.deltaTime);
     protected override void Walk()
     {
     }
