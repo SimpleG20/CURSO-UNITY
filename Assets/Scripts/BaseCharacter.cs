3647c374e9b46aed60c33fce805a540219af6ae7
Arthur Gonçalves <arthurga54@gmail.com>
2025-10-09 09:35:27 -0300

    Revert "Merge branch 'aula_01' of https://github.com/SimpleG20/CURSO-UNITY into aula_01"
    This reverts commit f082faf760b4133905291ffb219a4380166c5954, reversing
    changes made to dd95ca10ef10409c21c18a4b05430f229b0f9f45.

--- Assets/Scripts/BaseCharacter.cs
+++ Assets/Scripts/BaseCharacter.cs
@@ -5,15 +5,14 @@ public abstract class BaseCharacter : MonoBehaviour
     public string Name { get; protected set; }
     public int Health { get; protected set; }
 
-    [SerializeField] protected float m_Speed;
-    [SerializeField] protected float m_Strength;
-    [SerializeField] protected bool m_IsAlive;
+    [SerializeField] protected float m_speed;
+    [SerializeField] protected float m_strength;
+    [SerializeField] protected bool m_isAlive;
 
     protected void Awake()
     {
         Setup();
     }
-
     protected abstract void Setup();
 
 
